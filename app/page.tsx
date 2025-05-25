bd1201f0c0b2567305601ef7fbee22ff5d3574ee
Ifedayo Damilola <sureife@gmail.com>
2025-05-25 08:44:16 +0100

    header+hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Header from "@/sections/Header";
+import Hero from "@/sections/Hero";
+
 export default function Home() {
-  return <h1>Crafting digital experiences through code and creative design</h1>;
+  return <>
+    <Header />
+    <Hero />
+  </>
 }
