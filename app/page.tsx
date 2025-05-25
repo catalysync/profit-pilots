444e8ace99a969910c9715f01c3ec3dab9a9c822
Ifedayo Damilola <sureife@gmail.com>
2025-05-25 09:06:43 +0100

    intro section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/sections/Header";
 import Hero from "@/sections/Hero";
+import Intro from "@/sections/Intro";
 
 export default function Home() {
   return <>
     <Header />
     <Hero />
+    <Intro />
   </>
 }
