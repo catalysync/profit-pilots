4cd551b7de23ddc34a59837e92f2b317b1a78558
Ifedayo Damilola <sureife@gmail.com>
2025-05-29 06:41:06 +0100

    footer navigation

--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -1,32 +1,45 @@
+"use client"
 import Button from "@/components/Button";
-import { FC } from "react";
+import { FC, MouseEvent } from "react";
 
 const navItems = [
   {
-    href: '#',
-    label: "Home"
+    href: '#about',
+    label: "About"
   },
   {
-    href: '#',
+    href: '#projects',
     label: "Projects"
   },
   {
-    href: '#',
+    href: '#testimonials',
     label: "Testimonials"
   },
   {
-    href: '#',
+    href: '#faqs',
     label: "FAQs"
   },
   {
-    href: '#',
+    href: '#contact',
     label: "Contact"
   },
 ]
 
 const Footer: FC = () => {
+  const onClickNavItem = (e: MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+
+    const url = new URL(e.currentTarget.href);
+    const hash = url.hash;
+
+    const target = document.querySelector(hash);
+
+    if(!target) return;
+    target.scrollIntoView({ behavior: "smooth" })
+  }
+
   return (
-    <footer className="bg-stone-900 text-white">
+    <footer className="bg-stone-900 text-white" id="contact">
       <div className="container">
         <div className="section">
           <div className="flex items-center gap-3">
@@ -37,15 +50,29 @@ const Footer: FC = () => {
             <div className="md:col-span-2">
               <h2 className="text-4xl md:text-7xl lg:text-8xl mt-8 font-extralight">Mission parameters are set, Let&apos;s launch your vision.</h2>
               <Button variant="secondary" icon={
-                <svg 
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none" viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="size-6">
-                <path strokeLinecap="round"
-                strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
-              </svg>}
+                <div className="size-6 overflow-hidden">
+                  <div className="w-12 h-6 flex group-hover/button:-translate-x-1/2 transition-transform duration-500">
+                    <svg 
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none" viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="size-6">
+                      <path strokeLinecap="round"
+                      strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                    </svg>
+                    <svg 
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none" viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="size-6">
+                      <path strokeLinecap="round"
+                      strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                    </svg>
+                  </div>
+                </div>
+                }
               className="mt-8"
               >
                 info@profit+pilots.com
@@ -55,7 +82,7 @@ const Footer: FC = () => {
               <nav className="flex flex-col md:items-end gap-8 mt-16 md:mt-0">
                 {
                   navItems.map(({href, label}) => (
-                    <a href={href} key={label}>
+                    <a href={href} key={label} onClick={onClickNavItem} >
                       <Button variant="text" className="text-lg">{label}</Button>
                     </a>
                   ))
