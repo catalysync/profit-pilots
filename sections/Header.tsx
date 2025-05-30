d4dded32b2fb1ce6f3a8e4b13050f50a1e87772e
Ifedayo Damilola <sureife@gmail.com>
2025-05-30 15:19:07 +0100

    header overlay nav

--- sections/Header.tsx
+++ sections/Header.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import Button from "@/components/Button";
-import { FC } from "react";
+import { motion, useAnimate } from "framer-motion";
+import Link from "next/link";
+import { FC, useEffect, useState } from "react";
 
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 const navItems = [
@@ -26,26 +30,158 @@ const navItems = [
 ];
 
 const Header: FC = () => {
-  return <header className="fixed top-0 left-0 w-full backdrop-blur-md">
-    <div className="container !max-w-full">
-      <div className="flex items-center justify-between h-20">
-        <div>
-          <a href="/">
-            <span className="text-xl font-bold uppercase">
-              PROFIT+PILOTS
-            </span>
-          </a>
-        </div>
-        <div className="flex items-center gap-4">
-          <div className="size-11 border border-stone-400 rounded-full inline-flex items-center justify-center bg-stone-200">
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <rect x="3" y="15" width="18" height="2" fill="currentColor" />
-              <rect x="3" y="7" width="18" height="2" fill="currentColor" />
-            </svg>
+  const [isOpen, setIsOpen] = useState(false);
+  const [topLineScope, topLineAnimate] = useAnimate();
+  const [bottomLineScope, bottomLineAnimate] = useAnimate();
+  const [navScope, navAnimate] = useAnimate();
+
+  useEffect(() => {
+    if (isOpen) {
+      topLineAnimate([
+        [
+          topLineScope.current,
+          {
+            translateY: 4
+          }
+        ],
+        [
+          topLineScope.current,
+          {
+            rotate: 45
+          }
+        ]
+      ]);
+
+      bottomLineAnimate([
+        [
+          bottomLineScope.current,
+          {
+            translateY: -4
+          }
+        ],
+        [
+          bottomLineScope.current,
+          {
+            rotate: -45
+          }
+        ]
+      ]);
+
+      navAnimate(
+        navScope.current,
+        {
+          height: "100%",
+        },
+        {
+          duration: 0.7
+        }
+      );
+    } else {
+      topLineAnimate([
+        [
+          topLineScope.current,
+          {
+            rotate: 0
+          }
+        ],
+        [
+          topLineScope.current,
+          {
+            translateY: 0
+          }
+        ],
+      ]);
+
+      bottomLineAnimate([
+        [
+          bottomLineScope.current,
+          {
+            rotate: 0
+          }
+        ],
+        [
+          bottomLineScope.current,
+          {
+            translateY: 0
+          }
+        ],
+      ]);
+
+      navAnimate(
+        navScope.current,
+        {
+          height: 0,
+        },
+      );
+    }
+  }, [bottomLineAnimate, bottomLineScope, isOpen, navAnimate, navScope, topLineAnimate, topLineScope]);
+
+  return <header>
+    <div className="fixed top-0 left-0 w-full h-0 overflow-hidden bg-stone-900 z-[10]" ref={navScope}>
+      <nav className="mt-20 flex flex-col ">
+        {navItems.map(({ label, href }) => (  
+          <Link onClick={() => { setIsOpen(false) }} href={href} key={label} className="relative group/project text-stone-200 border border-t last:border-b border-stone-800 py-8">
+            <div className="absolute bottom-0 left-0 w-full
+              h-0 group-hover/project:h-full transition-all duration-700 bg-stone-800">
+            </div>
+            
+            <div className="relative container !max-w-full flex items-center justify-between">
+              <span className="text-3xl">{label}</span>
+              <svg 
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+              </svg>
+            </div>
+          </Link>
+        ))}
+      </nav>
+    </div>
+    <div className="fixed top-0 left-0 w-full backdrop-blur-md mix-blend-difference z-[20]">
+      <div className="container !max-w-full">
+        <div className="flex items-center justify-between h-20">
+          <div>
+            <Link href="/">
+              <span className="text-xl font-bold uppercase text-white">
+                PROFIT+PILOTS
+              </span>
+            </Link>
           </div>
-          <Button variant="primary" className="hidden md:inline-flex items-center">
-            Contact Us
-          </Button>
+        </div>
+      </div>
+    </div>
+    <div className="fixed top-0 left-0 w-full z-[20]">
+      <div className="container !max-w-full">
+        <div className="flex items-center justify-end h-20">
+          <div className="flex items-center gap-4">
+            <div className="size-11 border border-stone-400 rounded-full inline-flex items-center justify-center bg-stone-200" onClick={
+              () => setIsOpen(!isOpen)
+            }>
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <motion.rect
+                  x="3"
+                  y="7"
+                  width="18"
+                  height="2"
+                  fill="currentColor"
+                  ref={topLineScope}
+                />
+                <motion.rect 
+                  x="3"
+                  y="15"
+                  width="18"
+                  height="2"
+                  fill="currentColor"
+                  ref={bottomLineScope}
+                />
+              </svg>
+            </div>
+            <Button variant="primary" className="hidden md:inline-flex items-center">
+              Contact Us
+            </Button>
+          </div>
         </div>
       </div>
     </div>
--- sections/Projects.tsx
+++ sections/Projects.tsx
@@ -32,7 +32,7 @@ const projects = [
 
 const Projects: FC = () => {
   return (
-    <section className="section">
+    <section className="section" id="projects">
       <div className="container">
         <h2 className="text-4xl md:text-7xl lg:text-8xl">Orbital Missions</h2>
         <div className="mt-10 md:mt-16 lg:mt-20">
@@ -49,6 +49,25 @@ const Projects: FC = () => {
                 
                 <div className="mt-8  md:mt-0 flex justify-between items-center">
                   <h3 className="text-2xl md:text-3xl lg:text-4xl">{name}</h3>
+
+                  {/* <div className="size-6 overflow-hidden">
+                    <div className="h-6 w-12 flex group-hover/project:-translate-x-1.5 transition-transform duration-500">
+                      <svg 
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                        stroke="currentColor"
+                        className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                      </svg>
+                      <svg 
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                        stroke="currentColor"
+                        className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                      </svg>
+                    </div>
+                  </div> */}
                   <svg 
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
