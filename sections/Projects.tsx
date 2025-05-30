2f8465cdeb312925892a7d3c9cb3dc43b7d1464d
Ifedayo Damilola <sureife@gmail.com>
2025-05-30 19:21:54 +0100

    projects hover animations

--- sections/Projects.tsx
+++ sections/Projects.tsx
@@ -47,34 +47,43 @@ const Projects: FC = () => {
                   <Image src={image} alt={`${name} image`} className="size-full object-cover"/>
                 </div>
                 
-                <div className="mt-8  md:mt-0 flex justify-between items-center">
-                  <h3 className="text-2xl md:text-3xl lg:text-4xl">{name}</h3>
-
-                  {/* <div className="size-6 overflow-hidden">
-                    <div className="h-6 w-12 flex group-hover/project:-translate-x-1.5 transition-transform duration-500">
-                      <svg 
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
-                        stroke="currentColor"
-                        className="size-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
-                      </svg>
-                      <svg 
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
-                        stroke="currentColor"
-                        className="size-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
-                      </svg>
+                <div className="mt-8 md:mt-0 
+                  flex justify-between items-center 
+                  md:grid md:[grid-template-columns:1fr_300px_max-content]
+                  md:gap-8
+                ">
+                  <h3 className="text-2xl md:text-3xl lg:text-4xl lg:group-hover/project:pl-4 transition-all duration-700">{name}</h3>
+                  <div className="relative">
+                    <div className="absolute aspect-video w-full top-1/2 
+                      -translate-y-1/2 opacity-0 scale-90
+                      group-hover/project:opacity-100 group-hover/project:scale-110
+                      transition-all duration-500 z-10
+                      lg:group-hover/project:pr-8
+                    ">
+                      <Image src={image} alt={`${name} image`} className="size-full object-cover"/>
                     </div>
-                  </div> */}
-                  <svg 
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
-                    stroke="currentColor"
-                    className="size-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
-                  </svg>
+                  </div>
+
+                  <div className="lg:group-hover/project:pr-8 transition-all duration-700">
+                    <div className="size-6 overflow-hidden">
+                      <div className="h-6 w-12 flex group-hover/project:-translate-x-1/2 transition-transform duration-500">
+                        <svg 
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                          stroke="currentColor"
+                          className="size-6">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                        </svg>
+                        <svg 
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                          stroke="currentColor"
+                          className="size-6">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                        </svg>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </a>
